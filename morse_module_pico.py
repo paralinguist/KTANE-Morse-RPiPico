b81e0c21a58ae67822238a55c1011028769c4e58
Jonathan <paralinguist@users.noreply.github.com>
2022-10-10 18:44:38 +0800

    Additional button, serial host fixes
    Unclobbered serial file. Oop.

--- morse_module_pico.py
+++ morse_module_pico.py
@@ -1,14 +1,23 @@
 #Libraries from this article here:
 #https://www.circuitschools.com/interfacing-16x2-lcd-module-with-raspberry-pi-pico-with-and-without-i2c/
 from machine import Pin
-import utime
-import time
+from sys import stdin
+from machine import Timer
 from gpio_lcd import GpioLcd
+import select
+import time
 import random
 
+DISARMED = 0
+ARMED = 1
+
+status = DISARMED
+
 up_pin = 15
+down_pin = 12
 tx_pin = 13
 up_button = Pin(up_pin, Pin.IN, Pin.PULL_UP)
+down_button = Pin(down_pin, Pin.IN, Pin.PULL_UP)
 tx_button = Pin(tx_pin, Pin.IN, Pin.PULL_UP)
 led = Pin(14, Pin.OUT)
 
@@ -55,13 +64,21 @@ lcd = GpioLcd(rs_pin=Pin(16),
               d6_pin=Pin(20),
               d7_pin=Pin(21),
               num_lines=2, num_columns=16)
-  
+
+frequency = ""
+morse_word = ""
+
+def get_new_word():
+  global frequency, morse_word
+  frequency, morse_word = random.choice(list(WORD_LIST.items()))
 
 def display_message(message):
   lcd.clear()
   lcd.putstr(message)
 
 def display_disarmed():
+  global status
+  print('D', end='')
   lcd.clear()
   lcd.move_to(3,0)
   lcd.putstr('DISARMED')
@@ -70,36 +87,64 @@ def display_disarmed():
   happy_face = bytearray([0x00,0x0A,0x00,0x04,0x00,0x11,0x0E,0x00])
   lcd.custom_char(0, happy_face)
   lcd.putchar(chr(0))
+  status = DISARMED
+  get_new_word()
 
 def button_handler(pin):
   global up_button_last, tx_button_last, counter
   if pin == up_button:
     if time.ticks_diff(time.ticks_ms(), up_button_last) > 250:
+      counter += 1
       if counter >= len(FREQUENCIES):              
         counter = 0
-      else:
-        counter += 1
-        display_message(FREQUENCIES[counter] + " MHz")
-        up_button_last = time.ticks_ms()
-  elif pin == tx_button:
-    if time.ticks_diff(time.ticks_ms(), up_button_last) > 250:
+      display_message(FREQUENCIES[counter] + " MHz")
+      up_button_last = time.ticks_ms()
+  elif pin == tx_button and status == ARMED:
+    if time.ticks_diff(time.ticks_ms(), tx_button_last) > 500:
       if WORD_LIST[FREQUENCIES[counter]] == morse_word:
         display_disarmed()
       else:
-        display_message("WRONG")
+        lcd.move_to(0,1)
+        lcd.putstr('WRONG')
+        print('S', end='')
       tx_button_last = time.ticks_ms()
+  elif pin == down_button:
+    if time.ticks_diff(time.ticks_ms(), down_button_last) > 250:
+      counter -= 1
+      if counter < 0:              
+        counter = len(FREQUENCIES) - 1
+      display_message(FREQUENCIES[counter] + " MHz")
+      down_button_last = time.ticks_ms()
+
+#receives 1s if registered and active, 0s otherwise
+def server_checkin():
+  global status
+  if status == DISARMED:
+    print('!', end='')
+  poll = select.poll()
+  poll.register(stdin, select.POLLIN)
+  if poll.poll(0):
+    serial_input = stdin.read(1)
+    if serial_input == "1" and status == DISARMED:
+      status = ARMED
+      get_new_word()
+      display_message(FREQUENCIES[counter] + " MHz")
+    elif serial_input == "0":
+      status = DISARMED
+  poll.unregister(stdin)
 
 led.value(0)
-frequency, morse_word = random.choice(list(WORD_LIST.items()))
-print(morse_word)
+get_new_word()
 
 up_button.irq(trigger = machine.Pin.IRQ_RISING, handler = button_handler)
 tx_button.irq(trigger = machine.Pin.IRQ_RISING, handler = button_handler)
+serial_timer = Timer(period=3000, mode=Timer.PERIODIC, callback=lambda t:server_checkin())
 
-display_message(display_freq + " MHz")
+#display_message(display_freq + " MHz")
 
 while True:
-    for letter in morse_word:
+    if status == ARMED:
+      for letter in morse_word:
         for flash in MORSE[letter]:
           led.value(1)
           if flash == ".":
@@ -107,5 +152,9 @@ while True:
           elif flash == "-":
             time.sleep(DASH)
           led.value(0)
+          if status == DISARMED:
+            break
           time.sleep(INTRA_LETTER)
+        if status == DISARMED:
+            break
         time.sleep(INTER_LETTER)
